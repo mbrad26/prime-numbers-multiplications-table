647a57f0b2b736ed2f77fc21b7c5ecdb0d4563af
mbrad26 <mbrad26@gmail.com>
2020-11-30 20:33:01 +0000

    returns [2] if argument is 1

--- spec/generatePrimesArray.test.js
+++ spec/generatePrimesArray.test.js
@@ -4,4 +4,8 @@ describe('generatePrimesArray', () => {
   it('is defined', () => {
     expect(generatePrimesArray).toBeDefined();
   });
+
+  it('returns [2] if argument is 1', () => {
+    expect(generatePrimesArray(1)).toEqual([2]);
+  });
 });
