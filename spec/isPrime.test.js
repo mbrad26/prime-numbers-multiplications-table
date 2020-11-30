c9e9b7102ca98b926f50396288486158ff206f44
mbrad26 <mbrad26@gmail.com>
2020-11-30 11:27:00 +0000

    returns true if argument is 3

--- spec/isPrime.test.js
+++ spec/isPrime.test.js
@@ -2,10 +2,14 @@ const isPrime = require('../lib/isPrime');
 
 describe('isPrime', () => {
   it('is defined', () => {
-    expect(isPrime()).toBeDefined();
+    expect(isPrime).toBeDefined();
   });
 
   it('returns true if argument is 2', () => {
     expect(isPrime(2)).toBe(true);
   });
+
+  it('returns true if argument is 3', () => {
+    expect(isPrime(3)).toBe(true);
+  })
 });
