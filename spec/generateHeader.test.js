02032daf055dc7d9f85482bb6c4b43c5c88493fb
mbrad26 <mbrad26@gmail.com>
2020-12-01 13:38:19 +0000

    returns the correct header when argument is []

--- spec/generateHeader.test.js
+++ spec/generateHeader.test.js
@@ -4,4 +4,8 @@ describe.only('generateHeader', () => {
   it('is defined', () => {
     expect(generateHeader).toBeDefined();
   });
+
+  it('returns the correct header when argument is []', () => {
+    expect(generateHeader([])).toEqual("| |");
+  });
 });
