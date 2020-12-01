71ec400c7d40a35fa81252acaa0ae3123b68673d
mbrad26 <mbrad26@gmail.com>
2020-12-01 11:27:31 +0000

    refactor tests to account for isPrime mock

--- spec/generatePrimesArray.test.js
+++ spec/generatePrimesArray.test.js
@@ -9,7 +9,7 @@ describe('generatePrimesArray', () => {
   });
 
   afterEach(() => {
-    jest.clearAllMocks();
+    jest.resetAllMocks();
   });
 
   it('is defined', () => {
@@ -18,18 +18,25 @@ describe('generatePrimesArray', () => {
 
   it('returns [2] if argument is 1', () => {
     expect(generatePrimesArray(1)).toEqual([2]);
+    expect(isPrime).toHaveBeenCalledTimes(1);
+    expect(isPrime.mock.calls).toEqual([[2]]);
   });
 
   it('returns [2, 3] if argument is 2', () => {
+    isPrime.mockReturnValueOnce(true);
+
     expect(generatePrimesArray(2)).toEqual([2, 3]);
+    expect(isPrime).toHaveBeenCalledTimes(2);
+    expect(isPrime.mock.calls).toEqual([[2],[3]]);
   });
 
   it('returns [2, 3, 5] if argument is 3', () => {
     isPrime.mockReturnValueOnce(true)
            .mockReturnValueOnce(false)
-           .mockReturnValueOnce(true)
+           .mockReturnValueOnce(true);
 
     expect(generatePrimesArray(3)).toEqual([2, 3, 5]);
     expect(isPrime).toHaveBeenCalledTimes(4);
+    expect(isPrime.mock.calls).toEqual([[2],[3],[4],[5]]);
   });
 });
