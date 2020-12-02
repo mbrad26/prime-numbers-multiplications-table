ba70b80c78a411666d82ed13e605806fb6a4faa2
mbrad26 <mbrad26@gmail.com>
2020-12-02 12:27:30 +0000

    generates a row of multiples of 2 for a nested array of size 1

--- spec/generateTableRows.test.js
+++ spec/generateTableRows.test.js
@@ -4,4 +4,10 @@ describe.only('generateTableRows', () => {
   it('is defined', () => {
     expect(generateTableRows).toBeDefined();
   });
+
+  it('generates a row of multiples of 2 for a nested array of size 1', () => {
+    const array = [2];
+
+    expect(generateTableRows(array)).toEqual('| 2| 4|');
+  });
 });
