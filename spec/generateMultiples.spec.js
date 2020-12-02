3a28c49a715596bdbc7aade9a6eaf46c52797f8e
mbrad26 <mbrad26@gmail.com>
2020-12-02 15:11:20 +0000

    generateMultiples is defined

---
+++ spec/generateMultiples.spec.js
@@ -0,0 +1,7 @@
+const { generateMultiples } = require('../lib/generateMultiples');
+
+describe('generateMultiples', () => {
+  it('is defined', () => {
+    expect(generateMultiples).toBeDefined();
+  });
+});
