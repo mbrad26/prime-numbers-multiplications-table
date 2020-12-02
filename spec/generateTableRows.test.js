90bac5ee29b949e579d0e2e2979e46191c50ed3b
mbrad26 <mbrad26@gmail.com>
2020-12-02 11:59:03 +0000

    generateTableRows is defined

---
+++ spec/generateTableRows.test.js
@@ -0,0 +1,7 @@
+const { generateTableRows } = require('../lib/generateTableRows');
+
+describe.only('generateTableRows', () => {
+  it('is defined', () => {
+    expect(generateTableRows).toBeDefined();
+  });
+});
