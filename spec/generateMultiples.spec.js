4ea439205bd31c4792a774fbfc29202b3c1c188b
mbrad26 <mbrad26@gmail.com>
2020-12-02 15:18:58 +0000

    returns a nested array of multiples of 2 for [2]

--- spec/generateMultiples.spec.js
+++ spec/generateMultiples.spec.js
@@ -4,4 +4,8 @@ describe('generateMultiples', () => {
   it('is defined', () => {
     expect(generateMultiples).toBeDefined();
   });
+
+  it('returns a nested array of multiples of 2 for [2]', () => {
+    expect(generateMultiples([2])).toEqual([[2, 4]]);
+  });
 });
