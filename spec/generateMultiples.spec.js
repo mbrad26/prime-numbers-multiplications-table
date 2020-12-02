9a750edb74061ad0ae7a29024f362b4ca8b615e8
mbrad26 <mbrad26@gmail.com>
2020-12-02 19:18:20 +0000

    sanity check: returns a nested array of multiples of 2, 3 & 5 for [2, 3, 5]

--- spec/generateMultiples.spec.js
+++ spec/generateMultiples.spec.js
@@ -12,4 +12,8 @@ describe('generateMultiples', () => {
   it('returns a nested array of multiples of 2 & 3 for [2, 3]', () => {
     expect(generateMultiples([2, 3])).toEqual([[2, 4, 6], [3, 6, 9]]);
   });
+
+  it('returns a nested array of multiples of 2, 3 & 5 for [2, 3, 5]', () => {
+    expect(generateMultiples([2, 3, 5])).toEqual([[2, 4, 6, 10], [3, 6, 9, 15], [5, 10, 15, 25]]);
+  });
 });
