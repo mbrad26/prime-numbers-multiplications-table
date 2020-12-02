232b8efb17687f9a4bf420aa893d0010f6ef12ea
mbrad26 <mbrad26@gmail.com>
2020-12-02 13:56:24 +0000

    refactor the test suit and generateTableRows function

--- spec/generateTableRows.test.js
+++ spec/generateTableRows.test.js
@@ -1,13 +1,22 @@
 const { generateTableRows } = require('../lib/generateTableRows');
 
 describe.only('generateTableRows', () => {
+  let generateMultiples;
+
+  beforeEach(() => {
+    generateMultiples = jest.fn();
+  });
+
   it('is defined', () => {
     expect(generateTableRows).toBeDefined();
   });
 
-  it('generates a row of multiples of 2 for a nested array of size 1', () => {
+  it('generates a row of multiples of 2 for [2]', () => {
     const array = [2];
+    generateMultiples.mockReturnValueOnce([[2, 4]]);
 
-    expect(generateTableRows(array)).toEqual('| 2| 4|');
+    expect(generateTableRows(array, generateMultiples)).toEqual('| 2| 4|');
+    expect(generateMultiples).toHaveBeenCalledTimes(1);
+    expect(generateMultiples).toHaveBeenCalledWith([2]);
   });
 });
