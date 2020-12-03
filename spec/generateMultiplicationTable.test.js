b59b345cdb8ee9dc16a2f902edf7b21c85157585
mbrad26 <mbrad26@gmail.com>
2020-12-03 12:55:13 +0000

    refactor test suit and implementation for generateMultiplicationTable

--- spec/generateMultiplicationTable.test.js
+++ spec/generateMultiplicationTable.test.js
@@ -14,9 +14,15 @@ describe('generateMultiplicationTable', () => {
 
   it('generates a multiplication table when input is 1', () => {
     generatePrimesArray.mockReturnValueOnce([2]);
-    generateHeader.mockReturnValueOnce('| | 2|');
+    generateHeader.mockReturnValueOnce('| | 2|\n');
     generateTableRows.mockReturnValueOnce('| 2| 4|\n')
 
     expect(generateMultiplicationTable(1)).toEqual('| | 2|\n| 2| 4|\n');
+    expect(generatePrimesArray).toHaveBeenCalledTimes(1);
+    expect(generateHeader).toHaveBeenCalledTimes(1);
+    expect(generateTableRows).toHaveBeenCalledTimes(1);
+    expect(generatePrimesArray).toHaveBeenCalledWith(1);
+    expect(generateHeader).toHaveBeenCalledWith([2]);
+    expect(generateTableRows).toHaveBeenCalledWith([2]);
   });
 });
