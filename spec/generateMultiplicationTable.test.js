378c474895e7d9f1f131f68f9b6ba256ec9baedf
mbrad26 <mbrad26@gmail.com>
2020-12-03 13:34:31 +0000

    generates a multiplication table when input is 3

--- spec/generateMultiplicationTable.test.js
+++ spec/generateMultiplicationTable.test.js
@@ -8,6 +8,10 @@ jest.mock('../lib/generateHeader');
 jest.mock('../lib/generateTableRows');
 
 describe('generateMultiplicationTable', () => {
+  afterEach(() => {
+    jest.resetAllMocks()
+  });
+
   it('is defined', () => {
     expect(generateMultiplicationTable).toBeDefined();
   });
@@ -24,5 +28,24 @@ describe('generateMultiplicationTable', () => {
     expect(generatePrimesArray).toHaveBeenCalledWith(1);
     expect(generateHeader).toHaveBeenCalledWith([2]);
     expect(generateTableRows).toHaveBeenCalledWith([2]);
+  });
+
+  it('generates a multiplication table when input is 3', () => {
+    let array  = [2, 3, 5];
+    let header = '|   |  2|  3|  5|\n';
+    let rows   = '|  2|  4|  6| 10|\n|  3|  6|  9| 15|\n|  5| 10| 15| 25|\n';
+    let result = '|   |  2|  3|  5|\n|  2|  4|  6| 10|\n|  3|  6|  9| 15|\n|  5| 10| 15| 25|\n'
+
+    generatePrimesArray.mockReturnValueOnce(array);
+    generateHeader.mockReturnValueOnce(header);
+    generateTableRows.mockReturnValueOnce(rows);
+
+    expect(generateMultiplicationTable(3)).toEqual(result);
+    expect(generatePrimesArray).toHaveBeenCalledTimes(1);
+    expect(generateHeader).toHaveBeenCalledTimes(1);
+    expect(generateTableRows).toHaveBeenCalledTimes(1);
+    expect(generatePrimesArray).toHaveBeenCalledWith(3);
+    expect(generateHeader).toHaveBeenCalledWith([2, 3, 5]);
+    expect(generateTableRows).toHaveBeenCalledWith([2, 3, 5]);
   });
 });
