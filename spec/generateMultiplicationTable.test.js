acc5c41bbf8e890bf968547e68cf88e4d26c92e9
mbrad26 <mbrad26@gmail.com>
2020-12-03 12:15:32 +0000

    generates a multiplication table when input is 1

--- spec/generateMultiplicationTable.test.js
+++ spec/generateMultiplicationTable.test.js
@@ -4,4 +4,8 @@ describe('generateMultiplicationTable', () => {
   it('is defined', () => {
     expect(generateMultiplicationTable).toBeDefined();
   });
+
+  it('generates a multiplication table when input is 1', () => {
+    expect(generateMultiplicationTable(1)).toEqual('| | 2|\n| 2| 4|\n');
+  });
 });
