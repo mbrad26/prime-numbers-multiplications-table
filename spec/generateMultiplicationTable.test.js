48f36682e70c95b43f15ec7cbc3cf9fc75a42ab8
mbrad26 <mbrad26@gmail.com>
2020-12-03 14:43:00 +0000

    refactor test suits and implementations for generateTableRows, generateMultiplicationTable and generateHeader; padding is no longer exported but is returned from generateTableRows

--- spec/generateMultiplicationTable.test.js
+++ spec/generateMultiplicationTable.test.js
@@ -17,35 +17,38 @@ describe('generateMultiplicationTable', () => {
   });
 
   it('generates a multiplication table when input is 1', () => {
+    let padding = 1;
+
     generatePrimesArray.mockReturnValueOnce([2]);
     generateHeader.mockReturnValueOnce('| | 2|\n');
-    generateTableRows.mockReturnValueOnce('| 2| 4|\n')
+    generateTableRows.mockReturnValueOnce({ 'rows': '| 2| 4|\n', padding: 1 });
 
     expect(generateMultiplicationTable(1)).toEqual('| | 2|\n| 2| 4|\n');
     expect(generatePrimesArray).toHaveBeenCalledTimes(1);
     expect(generateHeader).toHaveBeenCalledTimes(1);
     expect(generateTableRows).toHaveBeenCalledTimes(1);
     expect(generatePrimesArray).toHaveBeenCalledWith(1);
-    expect(generateHeader).toHaveBeenCalledWith([2]);
+    expect(generateHeader).toHaveBeenCalledWith([2], padding);
     expect(generateTableRows).toHaveBeenCalledWith([2]);
   });
 
   it('generates a multiplication table when input is 3', () => {
     let array  = [2, 3, 5];
+    let padding = 2;
     let header = '|   |  2|  3|  5|\n';
     let rows   = '|  2|  4|  6| 10|\n|  3|  6|  9| 15|\n|  5| 10| 15| 25|\n';
-    let result = '|   |  2|  3|  5|\n|  2|  4|  6| 10|\n|  3|  6|  9| 15|\n|  5| 10| 15| 25|\n'
+    let result = '|   |  2|  3|  5|\n|  2|  4|  6| 10|\n|  3|  6|  9| 15|\n|  5| 10| 15| 25|\n';
 
     generatePrimesArray.mockReturnValueOnce(array);
     generateHeader.mockReturnValueOnce(header);
-    generateTableRows.mockReturnValueOnce(rows);
+    generateTableRows.mockReturnValueOnce({ rows, padding });
 
     expect(generateMultiplicationTable(3)).toEqual(result);
     expect(generatePrimesArray).toHaveBeenCalledTimes(1);
     expect(generateHeader).toHaveBeenCalledTimes(1);
     expect(generateTableRows).toHaveBeenCalledTimes(1);
     expect(generatePrimesArray).toHaveBeenCalledWith(3);
-    expect(generateHeader).toHaveBeenCalledWith([2, 3, 5]);
+    expect(generateHeader).toHaveBeenCalledWith([2, 3, 5], padding);
     expect(generateTableRows).toHaveBeenCalledWith([2, 3, 5]);
   });
 });
--- spec/generateTableRows.test.js
+++ spec/generateTableRows.test.js
@@ -14,39 +14,46 @@ describe.only('generateTableRows', () => {
 
   it('generates a row with multiples of 2 for [2]', () => {
     const array = [2];
+    const padding = 1;
+    const rows = '| 2| 4|\n';
+
     generateMultiples.mockReturnValueOnce([[2, 4]]);
 
-    expect(generateTableRows(array)).toEqual('| 2| 4|\n');
+    expect(generateTableRows(array)).toEqual({ rows, padding });
     expect(generateMultiples).toHaveBeenCalledTimes(1);
     expect(generateMultiples).toHaveBeenCalledWith([2]);
   });
 
   it('adds new line after each row', () => {
     const array = [2];
+    const padding = 1;
+    const rows = '| 2| 4|\n';
 
     generateMultiples.mockReturnValueOnce([[2, 4]]);
 
-    expect(generateTableRows(array)).toEqual('| 2| 4|\n');
+    expect(generateTableRows(array)).toEqual({ rows, padding });
   });
 
   it('generates two rows with multiples of 2 & 3 for [2, 3]', () => {
     const array = [2, 3];
-    const result = '| 2| 4| 6|\n| 3| 6| 9|\n';
+    const padding = 1;
+    const rows = '| 2| 4| 6|\n| 3| 6| 9|\n';
 
     generateMultiples.mockReturnValueOnce([[2, 4, 6], [3, 6, 9]]);
 
-    expect(generateTableRows(array)).toEqual(result);
+    expect(generateTableRows(array)).toEqual({ rows, padding });
     expect(generateMultiples).toHaveBeenCalledTimes(1);
     expect(generateMultiples).toHaveBeenCalledWith([2, 3]);
   });
 
   it('generates three rows with multiples of 2, 3 & 5 for [2, 3, 5]', () => {
     const array = [2, 3, 5];
-    const result = '|  2|  4|  6| 10|\n|  3|  6|  9| 15|\n|  5| 10| 15| 25|\n';
+    const padding = 2;
+    const rows = '|  2|  4|  6| 10|\n|  3|  6|  9| 15|\n|  5| 10| 15| 25|\n';
 
     generateMultiples.mockReturnValueOnce([[2, 4, 6, 10], [3, 6, 9, 15], [5, 10, 15, 25]]);
 
-    expect(generateTableRows(array)).toEqual(result);
+    expect(generateTableRows(array)).toEqual({ rows, padding });
     expect(generateMultiples).toHaveBeenCalledTimes(1);
     expect(generateMultiples).toHaveBeenCalledWith([2, 3, 5]);
   });
